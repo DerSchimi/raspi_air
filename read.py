6f6a64532e655e4e04631dd431c0e5ba45ee3c99
Daniel Schimikowski <daniel.schimikowski@gmail.com>
2024-12-14 20:58:32 +0100

    Revert "Add web server to display chart"
    This reverts commit 70ea6bf13a1f9a8365e09f9276d4d946df99f0e0.

--- read.py
+++ read.py
@@ -9,15 +9,10 @@ from pms.core import SensorReader
 import datetime
 import time
 
-def format_time(timestamp):
-    return datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
-
 def create_logfile():
     current_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
     filename = f"log_{current_time}.csv"
-    logfile = open(filename, 'w')
-    logfile.write("time, pm25, pm10\n")
-    return logfile
+    return open(filename, 'w')
 
 reader = SensorReader("SDS011", "/dev/ttyUSB0", interval=20, samples=4)
 
@@ -26,9 +21,12 @@ start_time = time.time()
 
 print("\nSDS011 4 samples on CSV format with header")
 with reader:
+    print_header = True
     for obs in reader():
-        formatted_time = format_time(time.time())
-        logfile.write(f"{formatted_time}, {obs.pm25}, {obs.pm10}\n")
+        if print_header:
+            logfile.write(f"{obs:header}\n")
+            print_header = False
+        logfile.write(f"{obs:csv}\n")
         if time.time() - start_time > 300:
             break
 
