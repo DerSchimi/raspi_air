70ea6bf13a1f9a8365e09f9276d4d946df99f0e0
Daniel Schimikowski <DerSchimi@users.noreply.github.com>
2024-12-14 20:11:30 +0100

    Add web server to display chart
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/DerSchimi/raspi_air?shareId=XXXX-XXXX-XXXX-XXXX).

--- read.py
+++ read.py
@@ -9,10 +9,15 @@ from pms.core import SensorReader
 import datetime
 import time
 
+def format_time(timestamp):
+    return datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+
 def create_logfile():
     current_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
     filename = f"log_{current_time}.csv"
-    return open(filename, 'w')
+    logfile = open(filename, 'w')
+    logfile.write("time, pm25, pm10\n")
+    return logfile
 
 reader = SensorReader("SDS011", "/dev/ttyUSB0", interval=20, samples=4)
 
@@ -21,12 +26,9 @@ start_time = time.time()
 
 print("\nSDS011 4 samples on CSV format with header")
 with reader:
-    print_header = True
     for obs in reader():
-        if print_header:
-            logfile.write(f"{obs:header}\n")
-            print_header = False
-        logfile.write(f"{obs:csv}\n")
+        formatted_time = format_time(time.time())
+        logfile.write(f"{formatted_time}, {obs.pm25}, {obs.pm10}\n")
         if time.time() - start_time > 300:
             break
 
